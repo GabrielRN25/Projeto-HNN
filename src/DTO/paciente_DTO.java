ca557753d9bae22d214e913fcf079c84fe121506
Gabriel <163174639+GabrielRN25@users.noreply.github.com>
2024-09-27 12:36:32 -0300

    Base da beta

---
+++ src/DTO/paciente_DTO.java
@@ -0,0 +1,14 @@
+package DTO;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author gabri
+ */
+public class paciente_DTO {
+    
+}
